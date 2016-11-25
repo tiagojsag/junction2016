62b6cc96a0c0b87a6c0783b34705c055ffdf5555
Tiago Garcia <tiagojsag@gmail.com>
2016-11-25 19:27:51 +0100

    Basic parking page list

--- app/controllers/parking_places_controller.rb
+++ app/controllers/parking_places_controller.rb
@@ -1,5 +1,10 @@
 class ParkingPlacesController < InheritedResources::Base
 
+  def index
+    parking_places = ParkingPlace.paginate(:page => params[:page], :per_page => params[:per_page]).order(params[:order] || 'created_at ASC')
+    render json: parking_places
+  end
+
   private
 
     def parking_place_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 AdminUser.delete_all
+ParkingPlace.delete_all
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+for i in 0..100
+  ParkingPlace.create!(title: "Parking place #{i}",
+                       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse maximus nibh et libero tempus, et mattis quam blandit',
+                       latitude: Random.rand(-85..85),
+                       longitude: Random.rand(-180..180)
+  )
+end
