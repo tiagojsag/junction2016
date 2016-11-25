ec1684a63117f4e04ac4b76bc3b067c6f220d6f5
Tiago Garcia <tiagojsag@gmail.com>
2016-11-25 19:49:50 +0100

    Add required params for lat and long

--- app/controllers/parking_places_controller.rb
+++ app/controllers/parking_places_controller.rb
@@ -1,14 +1,18 @@
 class ParkingPlacesController < InheritedResources::Base
 
   def index
+    return render status: :bad_request, json: {message: "Missing required param 'latitude'."} if params[:latitude].blank?
+    return render status: :bad_request, json: {message: "Missing required param 'longitude'."} if params[:longitude].blank?
+
+
     parking_places = ParkingPlace.paginate(:page => params[:page], :per_page => params[:per_page]).order(params[:order] || 'created_at ASC')
     render json: parking_places
   end
 
   private
 
-    def parking_place_params
-      params.require(:parking_place).permit(:title, :description, :latitude, :longitude)
-    end
+  def parking_place_params
+    params.require(:parking_place).permit(:title, :description, :latitude, :longitude)
+  end
 end
 
